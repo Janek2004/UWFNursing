21576571f09b48d94aeba36ef0aef4cbc24e8522
Janusz Chudzynski <appzzman>
2015-04-03 11:38:35 -0500

    Changed to model and etc

--- Proximity.swift
+++ Proximity.swift
@@ -2,17 +2,18 @@
 //  Proximity.swift
 //  UWFNursing
 //
-//  Created by sadmin on 2/23/15.
+//  Created by Janusz Chudzynski on 2/23/15.
 //  Copyright (c) 2015 Janusz Chudzynski. All rights reserved.
 //
 
 import Foundation
 import CoreData
+@objc(Proximity)
 
-@objc class Proximity: NSManagedObject {
+class Proximity: NSManagedObject {
 
-    @NSManaged var timestamp: NSDate
-    @NSManaged var proximity: NSNumber
     @NSManaged var beaconid: String
+    @NSManaged var proximity: NSNumber
+    @NSManaged var timestamp: NSDate
 
 }
